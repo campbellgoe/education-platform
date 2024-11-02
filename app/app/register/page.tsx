8fa44fcd22de2e83cab40f14f86ebd6e0ee34b19
George Campbell <campbell.goe@gmail.com>
2024-11-02 16:43:35 +0000

    add confirm password

--- app/app/register/page.tsx
+++ app/app/register/page.tsx
@@ -11,15 +11,28 @@ import { useRouter } from 'next/navigation'
 export default function Page() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
   const [userType, setUserType] = useState('student')
+  const [error, setError] = useState('')
   const { register } = useAppContext()
   const router = useRouter()
   const handleSubmit = (e: React.FormEvent) => {
+    setError('')
     e.preventDefault()
+
+    if (password !== confirmPassword) {
+      setError("Passwords don't match")
+      return
+    }
     // alert("Not accepting registrations yet. Login with student@example.com or teacher1@example.com to preview your dashboard (logging you in as a "+userType+")")
     // login(userType === 'student' ? "student@example.com" : 'teacher1@example.com', password)
-    register(email, password, userType)
-    router.push('/app/dashboard')
+    try {
+      register(email, password, userType)
+      router.push('/app/dashboard')
+    } catch(err: any){
+      setError(err.message)
+    }
+    
   }
 
   return (
@@ -28,6 +41,7 @@ export default function Page() {
       <NavBarMain type={undefined}/>
       <div className="p-6 bg-white rounded shadow-md w-96">
         <h1 className="text-2xl font-bold mb-4">Register</h1>
+        {error && <p className="text-red-500 mb-4">{error}</p>}
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <Label htmlFor="email">Email</Label>
@@ -46,6 +60,16 @@ export default function Page() {
               id="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <Label htmlFor="confirmPassword">Confirm Password</Label>
+            <Input
+              type="password"
+              id="confirmPassword"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
               required
             />
           </div>
